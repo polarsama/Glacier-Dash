240469dc65262c4279607ef3c15c6056ed91377c
David Rincon <60829030+polarsama@users.noreply.github.com>
2026-07-26 02:07:44 -0500

    Update Obstacle.cs

--- Assets/Scritps/Obstacle.cs
+++ Assets/Scritps/Obstacle.cs
@@ -2,20 +2,26 @@ using UnityEngine;
 
 public class Obstacle : MonoBehaviour
 {
+    [Header("Obstacle Settings")]
     public float speedPenalty = 3f;
 
-    // DEBE decir OnTriggerEnter2D (con D al final)
-    // y el parametro DEBE ser Collider2D (con 2D al final)
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        Debug.Log("¡CONTACTO REGISTRADO CON: " + collision.gameObject.name + "!");
-
         PlayerController player = collision.GetComponent<PlayerController>();
 
         if (player != null)
         {
-            player.ApplySpeedPenalty(speedPenalty);
-            gameObject.SetActive(false);
+            if (player.IsDashing())
+            {
+                Debug.Log("¡ROCA DESTRUIDA CON DASH!");
+                // Aqui podremos agregar mas adelante un efecto visual o sonido de destruccion
+                gameObject.SetActive(false);
+            }
+            else
+            {
+                player.ApplySpeedPenalty(speedPenalty);
+                gameObject.SetActive(false);
+            }
         }
     }
 }
